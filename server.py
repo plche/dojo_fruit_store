04b32d156b9df2f2cbd260078fdab245cdbce465
plche <che.percy.l@gmail.com>
2022-01-22 18:15:06 -0500

    Fixed checkout.html

--- server.py
+++ server.py
@@ -8,7 +8,19 @@ def index():
 @app.route('/checkout', methods=['POST'])         
 def checkout():
     print(request.form)
-    return render_template("checkout.html")
+    payload = {
+        'strawberry' : int(request.form['strawberry']),
+        'raspberry' : int(request.form['raspberry']),
+        'blackberry' : int(request.form['blackberry']),
+        'apple' : int(request.form['apple']),
+        'first_name' : request.form['first_name'],
+        'last_name' : request.form['last_name'],
+        'student_id' : request.form['student_id'],
+    }
+    payload['customer_name'] = payload['first_name'] + " " + payload['last_name']
+    payload['count'] = payload['strawberry'] + payload['raspberry'] + payload['blackberry'] + payload['apple']
+    print(payload)
+    return render_template("checkout.html", payload=payload)
 
 @app.route('/fruits')         
 def fruits():
